ae1dabeb6ab3074006150d5dd634681a98747445
rismayanti8 <46665495+rismayanti8@users.noreply.github.com>
2020-01-22 10:13:19 +0700

    Tugas 1 Membuat Aplikasi Pengecekan BB
    Rismayanti

---
+++ ideal.php
@@ -0,0 +1,41 @@
+<?php 
+/**
+ * 
+ */
+class ideal{
+
+	function hitung($bmi, $jk){
+		if ($jk=="Laki-laki") {
+			if ($bmi<17) {
+				echo $bmi." || Under Weight/Kurus";
+			} elseif ($bmi>=17 && $bmi<=25) {
+				echo $bmi." || Normal Weight/Normal";
+			} elseif ($bmi>25 && $bmi<=27) {
+				echo $bmi." || Over Weight/Kegemukan";
+			} elseif ($bmi>27) {
+				echo $bmi." || Obesitas";
+			}
+		} elseif ($jk=="Perempuan") {
+			if ($bmi<18) {
+				echo $bmi." || Under Weight/Kurus";
+			} elseif ($bmi>=18 && $bmi<=25) {
+				echo $bmi." || Normal Weight/Normal";
+			} elseif ($bmi>25 && $bmi<=27) {
+				echo $bmi." || Over Weight/Kegemukan";
+			} elseif ($bmi>27) {
+				echo $bmi." || Obesitas";
+			}
+		}
+	}
+
+}
+$risma = new ideal(); 
+if (isset($_POST['submit'])) {
+	$nama=$_POST['nama'];
+	$bb= $_POST['bb'];
+	$tb = $_POST['tb'];
+	$jk = $_POST['jk'];
+	$bmi = $bb/($tb*$tb/10000);
+	$risma->hitung($bmi, $jk);
+}
+ ?>
